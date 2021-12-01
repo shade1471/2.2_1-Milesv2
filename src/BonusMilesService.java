aeb74b2d3ff4c48cba636e88770ab365f87a53c0
Andrey Gribanov <shade1471@gmail.com>
2021-12-01 14:21:09 +0700

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int price){
+        // Стоимость одной бонусной мили
+        int cost=20;
+        // Расчет кол-ва бонусных миль
+        int miles = price / cost;
+        return miles;
+    }
+}
